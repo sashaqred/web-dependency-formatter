a071a81c1a4af035b0db7ebb8788665ee17388e7
sashaqred <sashaqred@ya.ru>
2020-04-17 13:51:32 +0300

    feat: add loading and reading json files utility

---
+++ src/DependencyFormatter/utils/jsonReader/jsonReader.ts
@@ -0,0 +1,17 @@
+export async function jsonReader(file: File | null | undefined): Promise<unknown> {
+  const uploadEvent = new Promise<ProgressEvent<FileReader>>((resolve, reject) => {
+    if (file) {
+      const fileReader = new FileReader();
+      fileReader.onload = resolve;
+      fileReader.onerror = reject;
+      fileReader.readAsText(file);
+    } else {
+      reject(Error('No file provided'));
+    }
+  });
+  const loadedFile = (await uploadEvent).target?.result;
+  if (typeof loadedFile !== 'string') {
+    throw Error('Bad file format');
+  }
+  return JSON.parse(loadedFile) as unknown;
+}
