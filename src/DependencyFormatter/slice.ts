f97a9f136e64b53e361f25be19a4125d09f63694
sashaqred <sashaqred@ya.ru>
2020-04-20 13:42:47 +0300

    feat: add selectors for dependency formatter

--- src/DependencyFormatter/slice.ts
+++ src/DependencyFormatter/slice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface DependencyFormatterState {
+export interface DependencyFormatterState {
   dependencies: Record<string, string>;
 }
 
