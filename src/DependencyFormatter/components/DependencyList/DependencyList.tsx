bd65e6699f64473732ac62b25a1208bbd92447b3
sashaqred <sashaqred@ya.ru>
2020-04-21 14:53:36 +0300

    refactor: set default empty value for dependencies

--- src/DependencyFormatter/components/DependencyList/DependencyList.tsx
+++ src/DependencyFormatter/components/DependencyList/DependencyList.tsx
@@ -5,8 +5,8 @@ interface DependencyListProps {
   dependencies?: Record<string, string>;
 }
 
-export function DependencyList({ dependencies }: DependencyListProps) {
-  const list = Object.entries(dependencies || {}).map(([name, version]) => (
+export function DependencyList({ dependencies = {} }: DependencyListProps) {
+  const list = Object.entries(dependencies).map(([name, version]) => (
     <DependencyListItem key={name} name={name} version={version} />
   ));
   return <ul>{list}</ul>;
