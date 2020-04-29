85c11e737b8d906aa046e865fcecad609cab69f3
Sasha Verbilo <sashaqred@ya.ru>
2020-04-29 16:22:38 +0300

    refactor: file selector improvements (#36)
    * refactor: split file upload logic to different component
    
    * refactor: simplify typings for file selector
    
    * refactor: rename field for file in FileSelector
    
    * refactor: add usage of useField instead of context methods
    
    * refactor: improve callback usage
    
    * refactor: improve validation for file upload

--- src/@modules/DependencyFormatter/components/FileSelector/FileSelector.tsx
+++ src/@modules/DependencyFormatter/components/FileSelector/FileSelector.tsx
@@ -1,31 +1,22 @@
-import React, { ChangeEvent, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { Form, Formik } from 'formik';
-import { Button, FormError, InputFile } from '@modules/Styles';
+import { Button } from '@modules/Styles';
 import { jsonReader } from '../../utils';
+import { FileUpload } from '../FileUpload';
 
 interface FileSelectorProps {
   onFileLoaded?: (json: object) => void;
 }
 
 interface FileSelectorFormValue {
-  package: File | undefined | null;
-}
-
-function validateFileSelector(formValue: FileSelectorFormValue) {
-  const errors: Partial<Record<keyof FileSelectorFormValue, string>> = {};
-  const isJsonMimeType = formValue.package?.type === 'application/json';
-  const isJsonExtension = /\.json$/i.test(formValue.package?.name || '');
-  if (!(isJsonMimeType || isJsonExtension)) {
-    errors.package = 'Please select JSON file.';
-  }
-  return errors;
+  packageFile: File | undefined;
 }
 
 export function FileSelector({ onFileLoaded }: FileSelectorProps) {
   const submitCallback = useCallback(
     async (value: FileSelectorFormValue) => {
       try {
-        const json = await jsonReader(value.package);
+        const json = await jsonReader(value.packageFile);
         onFileLoaded?.(json);
       } catch (error) {
         // TODO #2 Add error handling
@@ -37,28 +28,12 @@ export function FileSelector({ onFileLoaded }: FileSelectorProps) {
   );
 
   return (
-    <Formik
-      initialValues={{ package: undefined }}
-      validate={validateFileSelector}
-      onSubmit={submitCallback}
-    >
-      {({ setFieldValue, setFieldTouched }) => (
-        <Form>
-          <InputFile
-            name="package"
-            accept=".json, application/json"
-            onChange={(event: ChangeEvent<HTMLInputElement>) => {
-              const file = event.currentTarget.files?.[0];
-              setFieldTouched('package');
-              setFieldValue('package', file);
-            }}
-          />
-          <br />
-          <FormError name="package" />
-          <br />
-          <Button type="submit">Submit</Button>
-        </Form>
-      )}
+    <Formik initialValues={{ packageFile: undefined }} onSubmit={submitCallback}>
+      <Form>
+        <FileUpload name="packageFile" />
+        <br />
+        <Button type="submit">Submit</Button>
+      </Form>
     </Formik>
   );
 }
---
+++ src/@modules/DependencyFormatter/components/FileUpload/FileUpload.tsx
@@ -0,0 +1,36 @@
+import React, { ChangeEvent, useCallback } from 'react';
+import { InputFile, FormError } from '@modules/Styles';
+import { useField } from 'formik';
+
+interface FileUploadProps {
+  name: string;
+}
+
+function fileUploadValidator(packageFile: File) {
+  let error: string | undefined;
+  const isJsonMimeType = packageFile?.type === 'application/json';
+  const isJsonExtension = /\.json$/i.test(packageFile?.name || '');
+  if (!(isJsonMimeType || isJsonExtension)) {
+    error = 'Please select JSON file.';
+  }
+  return error;
+}
+
+export function FileUpload({ name }: FileUploadProps) {
+  const [field, , helpers] = useField({ name, validate: fileUploadValidator });
+  const changeCallback = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const file = event.currentTarget.files?.[0];
+      helpers.setTouched(true);
+      helpers.setValue(file);
+    },
+    [helpers],
+  );
+  return (
+    <>
+      <InputFile name={field.name} accept=".json, application/json" onChange={changeCallback} />
+      <br />
+      <FormError name={field.name} />
+    </>
+  );
+}
